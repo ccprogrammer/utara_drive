9434658f187b9fc391db53d732b0bb354ca12eca
Lubianca <lubianca.samora@gmail.com>
2022-11-11 10:59:47 +0700

    up

--- lib/ui/screen/detail_screen/detail_screen.dart
+++ lib/ui/screen/detail_screen/detail_screen.dart
@@ -1,3 +1,4 @@
+
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:photo_view/photo_view.dart';
@@ -16,28 +17,61 @@ class DetailScreen extends StatefulWidget {
 }
 
 class _DetailScreenState extends State<DetailScreen> {
+  PhotoViewScaleStateController scaleController =
+      PhotoViewScaleStateController();
+  PhotoViewController controller = PhotoViewController();
+
+  bool scaleAble = true;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    controller.addIgnorableListener(() {
+      var width = MediaQuery.of(context).size.width / 1000;
+      double? scale = controller.scale;
+      if (scale != null && scale < width) {
+        setState(() {
+          controller.scale = controller.initial.scale;
+        });
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final item = widget.data;
-
+// 392
     return Scaffold(
       appBar: appBar(),
+      backgroundColor: Colors.white,
       body: Consumer<GalleryProvider>(
         builder: (context, provider, _) {
-          return CachedNetworkImage(
-            imageUrl: item['image_url'],
-            imageBuilder: (context, imageProvider) => PhotoView(
-              backgroundDecoration:
-                  const BoxDecoration(color: MyTheme.colorWhite),
-              imageProvider: imageProvider,
-            ),
-            placeholder: (context, url) => Shimmer.fromColors(
-              baseColor: Colors.grey[500]!,
-              highlightColor: Colors.grey[300]!,
-              child: const Skeleton(),
-            ),
-            errorWidget: (context, url, error) =>
-                const Center(child: Icon(Icons.error)),
+          return Column(
+            children: [
+              Expanded(
+                child: CachedNetworkImage(
+                  imageUrl: item['image_url'],
+                  imageBuilder: (context, imageProvider) => PhotoView(
+                    basePosition: Alignment.topCenter,
+                    scaleStateChangedCallback: (value) {},
+                    controller: controller,
+                    scaleStateController: scaleController,
+                    backgroundDecoration:
+                        const BoxDecoration(color: MyTheme.colorWhite),
+                    imageProvider: imageProvider,
+                    tightMode: true,
+                  ),
+                  placeholder: (context, url) => Shimmer.fromColors(
+                    baseColor: Colors.grey[500]!,
+                    highlightColor: Colors.grey[300]!,
+                    child: const Skeleton(),
+                  ),
+                  errorWidget: (context, url, error) =>
+                      const Center(child: Icon(Icons.error)),
+                ),
+              ),
+            ],
           );
         },
       ),
