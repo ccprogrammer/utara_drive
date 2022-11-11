715f28716ef215c28097464c843cfe2a588a07ca
Lubianca <lubianca.samora@gmail.com>
2022-11-11 11:24:05 +0700

    up

--- lib/ui/screen/detail_screen/detail_screen.dart
+++ lib/ui/screen/detail_screen/detail_screen.dart
@@ -1,4 +1,3 @@
-
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:photo_view/photo_view.dart';
@@ -22,55 +21,97 @@ class _DetailScreenState extends State<DetailScreen> {
   PhotoViewController controller = PhotoViewController();
 
   bool scaleAble = true;
+  bool seeDetail = false;
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-    controller.addIgnorableListener(() {
-      var width = MediaQuery.of(context).size.width / 1000;
-      double? scale = controller.scale;
-      if (scale != null && scale < width) {
-        setState(() {
-          controller.scale = controller.initial.scale;
-        });
-      }
-    });
+    // controller.addIgnorableListener(() {
+    //   var width = MediaQuery.of(context).size.width / 1000;
+    //   double? scale = controller.scale;
+    //   if (scale != null && scale < width) {
+    //     setState(() {
+    //       controller.scale = controller.initial.scale;
+    //     });
+    //   }
+    // });
   }
 
   @override
   Widget build(BuildContext context) {
     final item = widget.data;
-// 392
     return Scaffold(
-      appBar: appBar(),
       backgroundColor: Colors.white,
+      appBar: appBar(),
+      bottomNavigationBar: BottomAppBar(
+        color: MyTheme.colorWhite,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.edit),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.share),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.add_to_photos),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.delete),
+            ),
+            IconButton(
+              onPressed: () {
+                setState(() {
+                  seeDetail = !seeDetail;
+                });
+              },
+              icon: Icon(
+                  seeDetail ? Icons.arrow_circle_down : Icons.arrow_circle_up),
+            ),
+          ],
+        ),
+      ),
       body: Consumer<GalleryProvider>(
         builder: (context, provider, _) {
-          return Column(
+          return Stack(
             children: [
-              Expanded(
-                child: CachedNetworkImage(
-                  imageUrl: item['image_url'],
-                  imageBuilder: (context, imageProvider) => PhotoView(
-                    basePosition: Alignment.topCenter,
-                    scaleStateChangedCallback: (value) {},
-                    controller: controller,
-                    scaleStateController: scaleController,
-                    backgroundDecoration:
-                        const BoxDecoration(color: MyTheme.colorWhite),
-                    imageProvider: imageProvider,
-                    tightMode: true,
-                  ),
-                  placeholder: (context, url) => Shimmer.fromColors(
-                    baseColor: Colors.grey[500]!,
-                    highlightColor: Colors.grey[300]!,
-                    child: const Skeleton(),
-                  ),
-                  errorWidget: (context, url, error) =>
-                      const Center(child: Icon(Icons.error)),
+              CachedNetworkImage(
+                imageUrl: item['image_url'],
+                imageBuilder: (context, imageProvider) => PhotoView(
+                  customSize: MediaQuery.of(context).size,
+                  minScale: PhotoViewComputedScale.contained * 1,
+                  maxScale: PhotoViewComputedScale.covered * 1.8,
+                  basePosition: Alignment.center,
+                  scaleStateChangedCallback: (value) {},
+                  controller: controller,
+                  scaleStateController: scaleController,
+                  backgroundDecoration:
+                      const BoxDecoration(color: MyTheme.colorWhite),
+                  imageProvider: imageProvider,
                 ),
+                placeholder: (context, url) => Shimmer.fromColors(
+                  baseColor: Colors.grey[500]!,
+                  highlightColor: Colors.grey[300]!,
+                  child: const Skeleton(),
+                ),
+                errorWidget: (context, url, error) =>
+                    const Center(child: Icon(Icons.error)),
               ),
+              seeDetail
+                  ? Positioned(
+                      bottom: 0,
+                      child: Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: 150,
+                        color: MyTheme.colorWhite.withOpacity(0.3),
+                      ),
+                    )
+                  : const SizedBox(),
             ],
           );
         },
