5cdf18e1f21ae18beb05204603bfa963579b0782
Lubianca <lubianca.samora@gmail.com>
2022-11-13 15:29:02 +0700

    up

--- lib/ui/screen/main_screen/home_tab/home_tab.dart
+++ lib/ui/screen/main_screen/home_tab/home_tab.dart
@@ -166,45 +166,92 @@ class _HomeTabState extends State<HomeTab> {
 
         return Container(
           margin: const EdgeInsets.fromLTRB(16, 0, 16, 0),
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  Icon(
-                    item['type'] == 'image'
-                        ? Icons.image
-                        : Icons.slow_motion_video_rounded,
-                    color: MyTheme.colorDarkerGrey,
-                    size: 24,
-                  ),
-                  const SizedBox(width: 8),
-                  Text(
-                    item['label'] != '' ? item['label'] : item['image_name'],
-                    style: const TextStyle(
-                      color: MyTheme.colorDarkerGrey,
-                    ),
-                  ),
-                ],
-              ),
-              CachedNetworkImage(
-                  imageUrl: item['image_url'],
-                  placeholder: (context, url) => Shimmer.fromColors(
-                        baseColor: Colors.grey[500]!,
-                        highlightColor: Colors.grey[300]!,
-                        child: const Skeleton(
+          child: CachedNetworkImage(
+              imageUrl: item['image_url'],
+              placeholder: (context, url) => Shimmer.fromColors(
+                    baseColor: Colors.grey[500]!,
+                    highlightColor: Colors.grey[300]!,
+                    child: Column(
+                      children: [
+                        Row(
+                          children: const [
+                            Skeleton(
+                              width: 20,
+                              height: 18,
+                              radius: 4,
+                              marginRight: 8,
+                            ),
+                            Skeleton(
+                              width: 130,
+                              height: 18,
+                              radius: 4,
+                            )
+                          ],
+                        ),
+                        const Skeleton(marginTop: 16),
+                        const Skeleton(
                           height: 150,
                           radius: 16,
                         ),
+                      ],
+                    ),
+                  ),
+              errorWidget: (context, url, error) => Column(
+                    children: [
+                      Row(
+                        children: [
+                          Icon(
+                            item['type'] == 'image'
+                                ? Icons.image
+                                : Icons.slow_motion_video_rounded,
+                            color: MyTheme.colorDarkerGrey,
+                            size: 24,
+                          ),
+                          const SizedBox(width: 8),
+                          Text(
+                            item['label'] != ''
+                                ? item['label']
+                                : item['image_name'],
+                            style: const TextStyle(
+                              color: MyTheme.colorDarkerGrey,
+                            ),
+                          ),
+                        ],
                       ),
-                  errorWidget: (context, url, error) => const SizedBox(
-                    height: 150,
-                    
-                    child: Center(
+                      Container(
+                        margin: const EdgeInsets.only(top: 16),
+                        width: MediaQuery.of(context).size.width,
+                        height: 150,
+                        child: const Center(
                           child: Icon(Icons.error),
                         ),
+                      ),
+                    ],
                   ),
-                  imageBuilder: (context, imageProvider) {
-                    return Container(
+              imageBuilder: (context, imageProvider) {
+                return Column(
+                  children: [
+                    Row(
+                      children: [
+                        Icon(
+                          item['type'] == 'image'
+                              ? Icons.image
+                              : Icons.slow_motion_video_rounded,
+                          color: MyTheme.colorDarkerGrey,
+                          size: 24,
+                        ),
+                        const SizedBox(width: 8),
+                        Text(
+                          item['label'] != ''
+                              ? item['label']
+                              : item['image_name'],
+                          style: const TextStyle(
+                            color: MyTheme.colorDarkerGrey,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Container(
                       margin: const EdgeInsets.only(top: 16),
                       width: MediaQuery.of(context).size.width,
                       height: 150,
@@ -261,10 +308,10 @@ class _HomeTabState extends State<HomeTab> {
                               : const SizedBox(),
                         ),
                       ),
-                    );
-                  }),
-            ],
-          ),
+                    ),
+                  ],
+                );
+              }),
         );
       },
     );
